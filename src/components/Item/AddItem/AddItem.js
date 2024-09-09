f4a28ee9da451a9b9e8cf9f2e2e15f6efc9be32c
Samuel Matias <hi@samuelematias.com>
2024-09-09 19:08:56 -0300

    feat(component): create AddItem component

---
+++ src/components/Item/AddItem/AddItem.js
@@ -0,0 +1,17 @@
+import { TouchableOpacity } from 'react-native'
+import { FontAwesome } from '@expo/vector-icons'
+import PropTypes from 'prop-types'
+
+const AddItem = ({ onPress }) => {
+  return (
+    <TouchableOpacity onPress={onPress}>
+      <FontAwesome name="plus-circle" size={24} color="green" />
+    </TouchableOpacity>
+  )
+}
+
+AddItem.propTypes = {
+  onPress: PropTypes.func.isRequired,
+}
+
+export default AddItem
