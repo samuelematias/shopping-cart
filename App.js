dcdcf73baf1c484db3bc2a4472d5e9bd8da06b09
Samuel Matias <hi@samuelematias.com>
2024-09-08 21:36:02 -0300

    init project

--- App.js
+++ App.js
@@ -1,20 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar'
+import { StyleSheet, Text, View } from 'react-native'
+import colors from './src/theme/colors'
+
+const styles = StyleSheet.create({
+  container: {
+    alignItems: 'center',
+    backgroundColor: colors.white,
+    flex: 1,
+    justifyContent: 'center',
+  },
+})
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Open up App.js to start working on your app, LOL!</Text>
+      {/* StatusBar 'style' prop is for text appearance (light or dark), not for CSS styles */}
+      <StatusBar style="auto" /* eslint-disable-line */ />
     </View>
-  );
+  )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
