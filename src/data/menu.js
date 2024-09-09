364d0a54b97b5fb01390c51b6c74da3e88abfe4c
Samuel Matias <hi@samuelematias.com>
2024-09-09 17:50:57 -0300

    chore(data): mock menu items

---
+++ src/data/menu.js
@@ -0,0 +1,39 @@
+export const menuItems = [
+  {
+    id: 'spicyFriedChicken',
+    name: 'Spicy Fried Chicken',
+    price: 5.99,
+  },
+  {
+    id: 'baconStreetDog',
+    name: 'Bacon Street Dog',
+    price: 9.99,
+  },
+  {
+    id: 'gyuudon',
+    name: 'Gyuudon',
+    price: 4.99,
+  },
+  {
+    id: 'tomahawkSteak',
+    name: '44 oz Tomahawk Steak',
+    price: 92.48,
+  },
+  {
+    id: 'spaghettiMeatball',
+    name: 'Spaghetti and Meatballs',
+    price: 7.99,
+  },
+  {
+    id: 'homerunBall',
+    name: 'Homerun Balls',
+    price: 2.5,
+  },
+  {
+    id: 'chickenSandwich',
+    name: 'Chicken Sandwich',
+    price: 3.99,
+  },
+]
+
+export default menuItems
